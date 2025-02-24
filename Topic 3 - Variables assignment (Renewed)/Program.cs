4e5b6322f66cd2cda4693bea083efc435f8550dc
701035287 <701035287@2260I-L29-026>
2025-02-24 13:34:24 -0500

    Add project files.

---
+++ Topic 3 - Variables assignment (Renewed)/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Topic_3___Variables_assignment__Renewed_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aron John E. Bautista Topic 3 - Variables Programming Assistant
+            Console.WriteLine("Part 1");
+            string course;
+
+            int myRoom = 29;
+            double price = 2.99;
+            course = "Computer Science!";
+            //These are the things i need or these things are the given
+            Console.WriteLine("This is room #" + myRoom);
+            Console.WriteLine("The price is $" + price);
+            Console.WriteLine("I am interested in " + course);
+            Console.WriteLine("");
+            //Part 1 done
+            Console.WriteLine("Part 2");
+            string name, graduationyear;
+
+            name = "Aron John E. Bautista";
+            graduationyear = "2027";
+            Console.WriteLine("My name is " + name + " and I'll graduate in " + graduationyear + ".");
+            Console.WriteLine("");
+            //Part 2 done
+
+            Console.WriteLine("Part 3:");
+            Console.WriteLine("r = C over 2π");
+            Console.WriteLine("");
+            //Part 3 done??
+            //Part 4
+            Console.WriteLine("Part 4");
+            Console.WriteLine("1 + 2 + 3 = " + (1 + 2 + 3));
+        }
+    }
+}
